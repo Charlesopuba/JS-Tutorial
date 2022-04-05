4aef01873bd203a29d04c6fe65cbaa4dd80250b4
charles opuba <charles.opuba@student.moringaschool.com>
2022-04-05 10:13:59 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//alert('hello');
+console.log('hello');
+//variables-used to store data//
+var b = 'smooth';
+console.log(b);
+
+
+var somenumber = 45;
+console.log(somenumber);
+
+
+document.getElementById('sometext').innerHTML = '45';
+
+var age = prompt('what is your age ?');
